b34a67789adc595c57237aa60bdb74736c5f113c
aaron <du.zhijun.sx@uneed.com>
2025-07-09 10:20:42 +0800

    计时器页面右边划分两部分，上面展示图表数据，下面展示其他内容

--- lib/widget/timer.dart
+++ lib/widget/timer.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:time_manager/widget/clock_widget.dart';
 
@@ -45,10 +46,44 @@ class _TimerWidgetState extends State<TimerWidget> {
             child: Container(width: _dividerWidth, color: Colors.grey.shade400),
           ),
         ),
-        Container(
-          decoration: BoxDecoration(color: Colors.blue),
-          width: (1 - _leftWidthRatio) * widget.width - _dividerWidth,
-          child: Center(child: Text("记录")),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(color: Colors.blue),
+            width: (1 - _leftWidthRatio) * widget.width - _dividerWidth,
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return Column(
+                  children: [
+                    // container 分为上下两部分
+                    Container(
+                      height: constraints.maxHeight * 0.4, // 内部Container高度为外部Container的50%
+                      padding: EdgeInsets.all(16),
+                      child: LineChart(LineChartData(
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: [
+                              FlSpot(1, 1),
+                              FlSpot(2, 2),
+                              FlSpot(3, 3),
+                            ],
+                          ),
+                        ]
+                      )),
+                    ),
+                    // 可以在这里添加其他组件占用剩余的50%空间
+                    Expanded(
+                      child: Container(
+                        color: Colors.lightBlue,
+                        child: Center(
+                          child: Text('其他内容区域', style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
         ),
       ],
     );
